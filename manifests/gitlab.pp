b4c2e94bf9c7cf69d693601d158a8ba911c9bad4
Sebastian Cole <sebastian.paris@gmail.com>
2013-01-08 13:07:26 +1100

    Changed gitlab module source, updated params for gitlab module.
    Changing gitlab module to upstream (sbadia/puppet-gitlab)
    Changed the parameter list to include new variable like (gitlabd_dbname, etc.)
    Added resource chaining to fix an issue where resources would be applied in the correct order.
    Typically the error would be:
    - mysql would be applied before an apt-get update
    - ruby 1.9.3 install runs before build-essential packages could be applied
    -

--- manifests/gitlab.pp
+++ manifests/gitlab.pp
@@ -1,48 +1,64 @@
- class {
-    'gitlab':
-      git_user          => 'git',
-      git_home          => '/home/git',
-      git_email         => 'git@example.com',
-      git_comment       => 'GIT control version',
-      # Default provider for ssh keys is 'source'
-      # you can use also   => 'puppet:///modules/gitlab/file'
-      # fileserving on http doesn't work yet
-      # (http://projects.puppetlabs.com/issues/5783)
-      # If you choose ssh_key_provider = 'content'
-      # you can use directly => 'ssh-rsa AAA...'
-      git_admin_pubkey  => '/tmp/vagrant-puppet/files/gitlab.pub',
-      git_admin_privkey => '/tmp/vagrant-puppet/files/gitlab',
-      gitlab_sources    => 'https://github.com/gitlabhq/gitlabhq.git',
-      gitlab_user       => 'gitlab',
-      gitlab_home       => '/home/gitlab',
-      gitlab_comment    => 'GITLab',
-      gitlab_dbtype     => 'mysql',
-      gitlab_branch     => 'stable',
-      db_user           => 'root',
-      db_password       => 'robot',
-      admin_user        => 'admin',
-      admin_password    => 'password',
-      admin_name        => 'bobot',
-      admin_email       => 'bobot@gmail.com',
-      ldap_enabled       => true,
-      ldap_host          => '9msn.net',
-      ldap_base          => 'OU=Accounts,DC=9msn,DC=net',
-      ldap_uid           => 'sAMAccountName',
-      ldap_port          => '389',
-      ldap_method        => 'plain',
-      ldap_bind_dn       => '',
-      ldap_bind_password => '',
-  }
+
+
+$gitlab_dbname  = 'gitlab_prod'
+$gitlab_dbuser  = 'labu'
+$gitlab_dbpwd   = 'labpass'
+
+
+class {
+  'gitlab':
+    git_user          => 'git',
+    git_home          => '/home/git',
+    git_email         => 'git@example.com',
+    git_comment       => 'GIT control version',
+    # Default provider for ssh keys is 'source'
+    # you can use also   => 'puppet:///modules/gitlab/file'
+    # fileserving on http doesn't work yet
+    # (http://projects.puppetlabs.com/issues/5783)
+    # If you choose ssh_key_provider = 'content'
+    # you can use directly => 'ssh-rsa AAA...'
+    git_admin_pubkey  => '/tmp/vagrant-puppet/files/gitlab.pub',
+    git_admin_privkey => '/tmp/vagrant-puppet/files/gitlab',
+    gitolite_sources  => 'https://github.com/gitlabhq/gitolite.git',
+    gitolite_branch   => 'gl-v320',
+    gitlab_sources    => 'https://github.com/gitlabhq/gitlabhq.git',
+    gitlab_branch     => 'stable',
+
+    gitlab_user       => 'gitlab',
+    gitlab_home       => '/home/gitlab',
+    gitlab_comment    => 'GITLab',
+
+    
+    
+    #FIXME mysql db not yet created, see https://github.com/sbadia/puppet-gitlab/issues/11
+    gitlab_dbtype     => 'mysql',
+    gitlab_dbname     => $gitlab_dbname,
+    gitlab_dbuser     => $gitlab_dbuser,
+    gitlab_dbpwd      => $gitlab_dbpwd,
+    ldap_enabled      => false,
+}
 
 class { 'mysql::server':
   config_hash => { 'root_password' => 'robot' },
 }
 database_user { "root@localhost":
   password_hash => mysql_password('robot'),
-  require => Class['mysql::server'],
+  require => [Class['mysql::server'],Class['mysql::config']],
 }
-# exec { 'apt-get update':
-#       command => '/usr/bin/apt-get update',
-# }
+mysql::db {
+  $gitlab_dbname:
+    ensure   => 'present',
+    charset  => 'utf8',
+    user     => $gitlab_dbuser,
+    password => $gitlab_dbpwd,
+    host     => 'localhost',
+    grant    => ['all'],
+    require => [Class['mysql::server'],Class['mysql::config']],
+}
+exec { 'update repository':
+      command => '/usr/bin/apt-get update',
+}
 
-Exec['apt-get update'] -> Class['mysql::server'] -> Class['mysql::config'] -> Class['gitlab']
+Exec['update repository'] -> Class['mysql::server'] -> Class['mysql::config'] 
+Class['mysql::config'] -> Database_user['root@localhost'] -> Mysql::Db[$gitlab_dbname] 
+Mysql::Db[$gitlab_dbname] -> Class['Gitlab::Debian_packages']
